e45a51bbfb0f10a96b25e4fdf180133cee17af77
Zhengxiangzhong <dylenzheng@gmail.com>
2019-04-06 13:08:08 +0800

    Create write_txt.py

---
+++ voc_deal/write_txt.py
@@ -0,0 +1,41 @@
+import os
+import random
+
+
+def _main():
+    trainval_percent = 0.1
+    train_percent = 0.9
+    xmlfilepath = '/home/zhex/data/yuncong/Annotations'
+    total_xml = os.listdir(xmlfilepath)
+
+    num = len(total_xml)
+    list = range(num)
+    tv = int(num * trainval_percent)
+    tr = int(tv * train_percent)
+    trainval = random.sample(list, tv)
+    train = random.sample(trainval, tr)
+
+    ftrainval = open('/home/zhex/data/yuncong/ImageSets/Main/trainval.txt', 'w')
+    ftest = open('/home/zhex/data/yuncong/ImageSets/Main/test.txt', 'w')
+    ftrain = open('/home/zhex/data/yuncong/ImageSets/Main/train.txt', 'w')
+    fval = open('/home/zhex/data/yuncong/ImageSets/Main/val.txt', 'w')
+
+    for i in list:
+        name = total_xml[i][:-4] + '\n'
+        if i in trainval:
+            ftrainval.write(name)
+            if i in train:
+                ftest.write(name)
+            else:
+                fval.write(name)
+        else:
+            ftrain.write(name)
+
+    ftrainval.close()
+    ftrain.close()
+    fval.close()
+    ftest.close()
+
+
+if __name__ == '__main__':
+    _main()
