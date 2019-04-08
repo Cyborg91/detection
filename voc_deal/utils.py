52ec46f2a074f6ab7f38d2333bacc7eea6a56d9d
Zhengxiangzhong <dylenzheng@gmail.com>
2019-04-08 20:50:20 +0800

    Update utils.py

--- voc_deal/utils.py
+++ voc_deal/utils.py
@@ -3,8 +3,8 @@ import os
 
 def read_csv(csv_path, pre_dir):
     '''
-    :param csv_path:csv文件路径
-    :param pre_dir: 图片数据所在的文件夹
+    :param csv_path:csv file_path
+    :param pre_dir: pic_dir
     :return:
     '''
     label_dict = {}
@@ -12,20 +12,38 @@ def read_csv(csv_path, pre_dir):
         reader = csv.reader(f)
         header = True
         for line in reader:
-            # 除去文件头
+            # delete file head
             if header:
                 header = False
                 continue
-            # 处理文件存储路径，当做标签
+            
             image_path = os.path.join(pre_dir, line[0])
-            # 处理后面的bbox
             bbox = []
 
             if line[1] is not None and len(line[1].strip()) > 0:
                 for i in line[1].split(';'):
                     if i is not None and len(i.strip()) > 0:
                         bbox.append(list(map(lambda x: round(float(x.strip())), i.split(' '))))
-            # 添加到label_dict
+            # add to label_dict
             label_dict.setdefault(image_path, bbox)
     return label_dict
+
+def write_csv(result_dict, out_path='zhe.csv'):
+    '''
+    :param result_dict: 
+    :param out_path:
+    :return:
+    '''
+    with open(out_path, 'w', newline='') as f:
+
+        for image in result_dict.keys():
+            writer = csv.writer(f)
+            image_name = os.path.split(image)[-1]
+            bbox = result_dict.get(image, [])
+            bbox_rs = ';'.join([' '.join(str(int(id)) for id in i) for i in bbox])
+            writer.writerow([image, bbox_rs])
+
+if __name__ == '__main__':
+    label_dict = read_csv(csv_path=r'./train.csv',pre_dir=r'/home/zhex/data')
+    write_csv(label_dict)
    
