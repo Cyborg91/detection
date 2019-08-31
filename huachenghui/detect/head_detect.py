7f4a0327e536dc2b3390f1cd62aba33c84aba295
scutzhe <scuteezxz@163com>
2019-08-31 10:13:19 +0800

    add resize_wrapper

--- huachenghui/detect/head_detect.py
+++ huachenghui/detect/head_detect.py
@@ -13,6 +13,7 @@ import numpy as np
 import time
 import requests
 from urllib.request import urlretrieve
+from threading import Lock
 
 import detect.denoise.pbcvt as denoise
 import detect.enhance.pbcvt as enhance
@@ -24,13 +25,47 @@ THRESHOLD
 花城汇球机轮巡获得超参
 更换场景后需要重新获取
 '''
-THRESHOLD = 135
+THRESHOLD = 120
+mutex = Lock()
+
+
+def resize_wrapper(ratio=0.8):
+    """
+    a resize wrapper, resize the input image, and return the correct location
+    add by shengyang at 2019.08.30 13:59
+    :param ratio: resize rate
+    :return:
+    """
+    def resize(func):
+        def function(*args, **kwargs):
+            """
+            注意，装饰的方法入第一个参数为输入opencv打开的图片，而且不能以关键字参数调用
+            若需要支持关键字参数调用，需要相应的此处
+            :param args:
+            :param kwargs:
+            :return:
+            """
+            img = args[0]
+            # print("in wrapper, ori size={}".format(img.shape))
+            new_img = cv2.resize(src=img, dsize=(round(img.shape[1]*ratio),
+                                                 round(img.shape[0]*ratio)),
+                                 interpolation=cv2.INTER_CUBIC)
+            # print("in wrapper, new size={}".format(new_img.shape))
+            ret = func(new_img)
+            # print("in wrapper, ori rect={}".format(ret['data']['headRect']))
+            ret['data']['headRect'] = [[round(ratio*i) for i in rect]
+                                       for rect in ret['data']['headRect']]
+            # print("in wrapper, new rect={}".format(ret['data']['headRect']))
+            return ret
+        return function
+    return resize
+
 
 def call_jeson(num,headRect):
     '''
     :param num: 人头数量
     :param headRect: 人头检测框坐标
-    :return: 以人头和人头检测框组合成的字典信息
+    :return: 人头数量和人头检测框组合成的字典信息
     '''
     call_json = {
         "code": 0,
@@ -41,6 +76,7 @@ def call_jeson(num,headRect):
     }
     return call_json;
 
+@resize_wrapper(0.8)
 def detect(img):
     '''
     功能：人头检测
@@ -48,7 +84,7 @@ def detect(img):
     return: 人头检测框和人头数量
     '''
     try:
-
+        mutex.acquire()	
         # 将BGR值转换为灰度值
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         pix_threshold= gray.sum()/gray.size
@@ -72,7 +108,7 @@ def detect(img):
             xmax = int(boxes[i][2])
             ymax = int(boxes[i][3])
             score = boxes[i][4]
-            if score < 0.15: # 0.15, 0.2, 0.3 场景不同，超参不同
+            if score < 0.15:
                 continue
             if xmin < 0:
                 xmin = 0
@@ -81,28 +117,28 @@ def detect(img):
             num = num + 1
 
             # 画矩形框
-            cv2.rectangle(img,(xmin,ymin),(xmax,ymax),(0,0,255),1)
+            # cv2.rectangle(img,(xmin,ymin),(xmax,ymax),(0,0,255),1)
 
             arr_num = [xmin,ymin,xmax,ymax]
             ret_data.append(arr_num)
 
 
         # 写入图片
-        cv2.imwrite('result_img/out_img.jpg',img)
+        # cv2.imwrite('result_img/'+'{}'.format(num)+'.jpg',img)
 
         # 打印人头数量
         print('num=',num)
 
         return_json = call_jeson(num,ret_data)
+        mutex.release()
         return return_json
 
     except Exception as e:
-        # 进入异常后，自动定位异常信息并打印
         import traceback
+        print('detection errors')
         err_information = traceback.format_exc()
-        traceback.print_ex()
-        cv2.imwrite('result_img/error_img.jpg', img)
+        traceback.print_exc()
         return {"code":1,
                 "msg":"system error",
-                "data":{"headRect":[]}}
-
+                "data":{"headRect":[],
+                        "err_information":"detection unknow error"}}
